c4812db01fa14a183dbea084ec053bcf6f0ef084
Vaibhav Nagpal <vaibhavnagpal.dev@gmail.com>
2024-12-14 21:08:29 +0530

    deployed links

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://course-management-weld-beta.vercel.app/',
     credentials: true
 }))
 
