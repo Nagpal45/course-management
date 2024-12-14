1e7b684568acc6932b80602361e4f3f0035691cc
Vaibhav Nagpal <vaibhavnagpal.dev@gmail.com>
2024-12-14 23:17:42 +0530

    server deploy on vercel

--- client/middleware.ts
+++ client/middleware.ts
@@ -4,21 +4,18 @@ import { jwtVerify } from 'jose';
 
 export async function middleware(request: NextRequest) {
     const token = request.cookies.get('token');
-    console.log(request.cookies);
     if (!token) {
         return NextResponse.redirect(new URL('/login', request.url));
     }
 
     try {
         const secret = process.env.JWT_SECRET;
-        console.log(secret);
         
         if (!secret) {
             throw new Error('JWT_SECRET is not defined');
         }
         
         const isValid = await jwtVerify(token.value, new TextEncoder().encode(secret));
-        console.log(isValid);
         if (!isValid) {
             throw new Error('Invalid token');
         }
